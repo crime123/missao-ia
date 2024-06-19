068b2fe6c24afcfee5d1e40e3de88cae1e4b1f92
crime123 <106694809+crime123@users.noreply.github.com>
2024-06-19 14:36:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,49 +4,83 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+function mostraResultadoo() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textcontent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
 const perguntas = [
     {
-        // texto: "assim que acabou o mundial, qual foi o brasileiro que foi o capeao muldia do rodeio PBR?",
-        afirmação: [
-            "Cassios dias barbosa!",
-            "Jose vitor lemes!"
+        enunciado: "Qual estilo vocẽ prefere?",
+        alternativas: [
+            {
+                texto: "Casual",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Despojado",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-        texto: " qual e o maior campenato brasileiro entre esses dois?"
-        afirmação: [
-            "ekipe rozeta",
-            "circuito rancho primavera",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-        texto: "qual o maior rodeio brasileiro?",
-        afirmação: [
-            "barretos",
-            "jaguariuna"
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-        texto: "quem foi o atual campeao de barretos no ano 2023?",
-        afirmação: [
-            "
-            
-           "warley oliveira"
-            "murilo henrique",
-            
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-        texto: "qual foi o primeiro rodeio do brasil?",
-        afirmação: [
-           "colorado.",
-            "barretos."
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "afirmação"
+            }
         ]
     },
 ];
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -54,23 +88,20 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-            const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click",() => respostasSelecionada(alternativa));
-            caixaAlternativas.appendChild(botaoAlternativas);
-           
-       function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historia += afirmacoes + "";
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
     atual++;
-    Let historia final = "";
+    mostraPergunta();
+}
 
-
+mostraPergunta();
